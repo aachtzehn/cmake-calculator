75fca4d70941a96b1fbe48fd7a025283571b625d
Andreas Achtzehn <andreas@achtzehn.de>
2020-07-27 22:00:10 +0200

    Local necessary changes

--- do_build.sh
+++ do_build.sh
@@ -1,10 +1,12 @@
-#!/bin/bash
+#!/bin/bash -x
 
 rm build/ -R
 mkdir build
 rm stage/ -R
 mkdir stage
 cd build
-cmake .. -DBUILD_SHARED_LIBS=On -DBUILD_CALCULATORLIB=On -DCMAKE_INSTALL_PREFIX=$(pwd)/../stage
+cmake .. -DBUILD_SHARED_LIBS=On -DBUILD_CALCULATORLIB=On -DCMAKE_INSTALL_PREFIX=$(pwd)/../stage -DBUILD_TESTING=On -DCMAKE_VERBOSE_MAKEFILE=On
 cmake --build .
-cmake -P cmake_install.cmake 
+ctest .
+cmake -P cmake_install.cmake
+
--- pkg/pg/conanfile.py
+++ pkg/pg/conanfile.py
@@ -2,22 +2,28 @@ from conans import ConanFile, CMake
 
 
 class CalcConan(ConanFile):
-    name = "calc"
+    name = "CalcLib"
     version = "1.0"
     license = "MIT License"
-    author = "Andreas Achtzehn andreas@achtzehn.de"
-    url = "https://github.com/aachtzehn/calc"
+    author = "Andreas Achtzehn <andreas@achtzehn.de>"
+    url = "https://github.com/aachtzehn/cmake-calculator"
     description = "Calculator example to test cmake and conan"
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False]}
-    default_options = {"shared": False}
+    default_options = {"shared": True}
     generators = "cmake"
-    exports_sources = "src/*","include/*"
+    exports_sources = "../../CMakeLists.txt*", "../../src*","../../include*","../../test*"
 
     def build(self):
-        cmake = CMake(self)
-        cmake.configure(source_folder="..")
+        cmake = CMake(self, parallel=True, set_cmake_flags=True)
+        # cmake .. -DBUILD_SHARED_LIBS=On -DBUILD_CALCULATORLIB=On -DCMAKE_INSTALL_PREFIX=$(pwd)/../stage -DBUILD_TESTING=On
+        cmake.verbose = True
+        cmake.definitions["BUILD_CALCULATORLIB"] = True
+        cmake.definitions["CMAKE_INSTALL_PREFIX"] = "stage/"
+        cmake.configure()
         cmake.build()
+        cmake.test()
+        cmake.install()
 
         # Explicit way:
         # self.run('cmake %s/hello %s'
@@ -25,7 +31,7 @@ class CalcConan(ConanFile):
         # self.run("cmake --build . %s" % cmake.build_config)
 
     def package(self):
-        self.copy("*", , src="stage", keep_path=True)
+        self.copy("stage/*", keep_path=True)
 
     def package_info(self):
         self.cpp_info.libs = ["CalcLib"]
